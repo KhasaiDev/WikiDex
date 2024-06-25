8247977706849f62a27e22b271b7e4dd21ab6563
KhasaiDev <khasaimassacre@gmail.com>
2024-06-25 17:53:49 -0400

    justProgress Commit

--- app/src/main/java/com/example/wikidex/view/MainActivity.kt
+++ app/src/main/java/com/example/wikidex/view/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.wikidex.view
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.wikidex.databinding.MainActivityBinding
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.wikidex.R
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +19,61 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         val navHostFragment=supportFragmentManager.findFragmentById(R.id.nav_host_Fragment) as NavHostFragment
         navController=navHostFragment.navController
+        setupActionBarWithNavController(navController)
+        this.setupDestinationChangedListener()
+    }
+    private fun setupDestinationChangedListener() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.lobbyFragment -> {
+                    supportActionBar?.title = "WikiDex!"
+                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                    supportActionBar?.setDisplayShowHomeEnabled(false)
+                }
+                R.id.regionesFragment -> {
+                    supportActionBar?.title = "Regiones!"
+                    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                    supportActionBar?.setDisplayShowHomeEnabled(true)
+                }
+                R.id.inicialesFragment2 -> {
+                    supportActionBar?.title = "Iniciales!"
+                    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                    supportActionBar?.setDisplayShowHomeEnabled(true)
+                }
+                else -> {
+                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                    supportActionBar?.setDisplayShowHomeEnabled(false)
+                }
+            }
+        }
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_logout -> {
+                navController.navigate(R.id.regionFragment_to_lobbyFragment)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        super.onPrepareOptionsMenu(menu)
+        val logoutItem = menu?.findItem(R.id.action_logout)
+
+        logoutItem?.isVisible = navController.currentDestination?.id == R.id.regionesFragment
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
